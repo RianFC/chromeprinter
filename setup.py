c48031c45a596ca86e727976487e4583b566492b
Rian Felipe Celestrino <39718272+RianFC@users.noreply.github.com>
2020-07-21 01:54:08 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='chromeprinter',
-    version='0.0.2',
+    version='0.0.3',
     packages=setuptools.find_packages(),
     url='',
     author='OLixao',
