b3a9329da624faf229b1cfff99345eb61d40a688
Rian Felipe Celestrino <39718272+RianFC@users.noreply.github.com>
2020-11-05 18:53:08 -0300

    Update __init__.py

--- chromeprinter/__init__.py
+++ chromeprinter/__init__.py
@@ -1,33 +1,24 @@
 import time
-from selenium import webdriver  
-from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 from PIL import Image
 
 class Client():
-    def __init__(self,CHROME_PATH='/usr/bin/google-chrome',CHROMEDRIVER_PATH='/usr/bin/chromedriver',WINDOW_SIZE = "1280,720"):
-        self.chromedr = CHROMEDRIVER_PATH
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--hide-scrollbars")
-        chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-        chrome_options.binary_location = CHROME_PATH
-        self.chrome_options = chrome_options
+    def __init__(self):
+        options = webdriver.FirefoxOptions()
+        options.add_argument('lang=pt-br')
+        options.headless = True
+        self.driver = webdriver.Firefox(
+            firefox_options=options)
 
     def make_screenshot(self,url, output):
-        driver = webdriver.Chrome(
-            executable_path=self.chromedr,
-            chrome_options=self.chrome_options
-            )
+        driver = self.driver
         driver.get(url)
         time.sleep(3)
         driver.save_screenshot(output)
         driver.close()
 
     def make_google(self, query):
-        driver = webdriver.Chrome(
-            executable_path=self.chromedr,
-            chrome_options=self.chrome_options
-            )
+        driver = self.driver
         driver.get('https://www.google.com/search?q='+query)
         S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
         driver.set_window_size(1080+S('Width'),720+S('Height'))
