6069b693c49c0d563ff29db41997f3211bab76bc
Rian Felipe Celestrino <39718272+RianFC@users.noreply.github.com>
2020-11-08 02:31:21 -0300

    Update __init__.py

--- chromeprinter/__init__.py
+++ chromeprinter/__init__.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from PIL import Image
+from urllib.parse import quote
 
 class Client():
     def __init__(self):
@@ -19,10 +20,17 @@ class Client():
 
     def make_google(self, query):
         driver = self.driver
-        driver.get('https://duckduckgo.com/?q='+query)
+        driver.get('https://duckduckgo.com/?q='+quote(query))
         S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
-        driver.set_window_size(1080+S('Width'),720+S('Height'))
-        element = driver.find_element_by_xpath('//*[@id="zero_click_wrapper"]')
+        driver.set_window_size(S('Width'),S('Height'))
+        a = ['//*[@id="zero_click_wrapper"]/div', '//*[@id="m1-0"]', '//*[@id="r1-0"]']
+        time.sleep(3)
+        for i in a:
+            try:
+                element = driver.find_element_by_xpath(i)
+                break
+            except Exception as e:
+                print(f'{i}: {e}')
         location = element.location
         size = element.size
         ctime = time.time()
